4f250f73c9a396d195954c8cbc845dbedd0d373a
Dylan Simon <dylan@dylex.net>
2008-11-02 02:31:16 -0500

    update for cabal 1.6
    darcs-hash:20081102073116-56c21-57827d86ebd0bfdab3e20893dc4a340e3a369e5f

--- Setup.hs
+++ Setup.hs
@@ -4,36 +4,37 @@ import Distribution.Simple.Setup
 import Distribution.Simple.Program
 import Distribution.Simple.LocalBuildInfo
 import Distribution.PackageDescription
+import Distribution.ModuleName (components)
 import Control.Monad
 import Data.Maybe
 import System.Directory
 import System.FilePath
-defhooks = defaultUserHooks
+defhooks = autoconfUserHooks
 programs = [
     simpleProgram "matlab", 
-    Program "mcr" (\_ -> return Nothing) (\_ _ -> return Nothing)
+    (simpleProgram "mcr") { programFindLocation = \_ -> return Nothing }
   ]
-runtime desc = maybe False (elem "Foreign.Matlab.Runtime" . exposedModules) $ library desc
+runtime desc = maybe False (elem ["Foreign","Matlab","Runtime"] . map components . exposedModules) $ library desc
 postconf args flags desc build = do
   confExists <- doesFileExist "configure"
-  unless confExists $ rawSystemPathExit verb "autoconf" []
+  unless confExists $ rawSystemExit verb "autoconf" []
   postConf defhooks args flags{ configConfigureArgs = configConfigureArgs flags ++ confargs } desc build
   where 
-    verb = configVerbose flags
+    verb = fromFlag $ configVerbosity flags
     confargs = ("--" ++ (if runtime desc then "enable" else "disable") ++ "-runtime") : map pconfarg pconf
     pconfarg p = "--with-" ++ programId p ++ "=" ++ programPath p ++ " " ++ unwords (programArgs p)
     pconf = mapMaybe (\p -> lookupProgram p (withPrograms build)) programs
 build desc binfo hooks flags = do
-  when (runtime desc) $ rawSystemPathExit (buildVerbose flags) "make" ["-Csrc"]
+  when (runtime desc) $ rawSystemExit (fromFlag $ buildVerbosity flags) "make" ["-Csrc"]
   buildHook defhooks desc binfo hooks flags
 clean desc binfo hooks flags = do
   makeExists <- doesFileExist "src/Makefile"
-  when makeExists $ rawSystemPathExit (cleanVerbose flags) "make" ["-Csrc", "clean"]
+  when makeExists $ rawSystemExit (fromFlag $ cleanVerbosity flags) "make" ["-Csrc", "clean"]
   cleanHook defhooks desc binfo hooks flags
 install desc binfo hooks flags = do
   instHook defhooks desc binfo hooks flags
   when (runtime desc) $ mapM_ (\f -> 
-      copyFileVerbose (installVerbose flags) 
+      copyFileVerbose (fromFlag $ installVerbosity flags) 
 	("src" </> f) 
 	(libdir (absoluteInstallDirs desc binfo NoCopyDest) </> f))
     ["libhsmatlab.so","libhsmatlab.ctf"]
@@ -44,7 +45,7 @@ reg desc binfo hooks flags = do
     lm l = l{ libBuildInfo = (libBuildInfo l)
 	{ ldOptions = ("-Wl,-rpath," ++ lib) : ldOptions (libBuildInfo l) } }
     lib
-      | regInPlace flags = pwd </> "src"
+      | fromFlag $ regInPlace flags = pwd </> "src"
       | otherwise = libdir (absoluteInstallDirs desc binfo NoCopyDest)
   regHook defhooks desc' binfo hooks flags
 hooks = defhooks {
